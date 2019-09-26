8a480d466c6c48129f1cd4199790fb8621c72069
tina.shi <tina.shi@gaiaworks.cn>
2019-09-26 15:01:39 +0800

    启用 Less

--- src/components/QuestionList/index.js
+++ src/components/QuestionList/index.js
@@ -1,29 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import './index.less';
+// import styled from "styled-components";
 
-const Ul = styled.ul`
-  padding: 0;
-  margin-top: 35px;
+// const Ul = styled.ul`
+//   padding: 0;
+//   margin-top: 35px;
 
-  li {
-    list-style: none;
-    background: #f6f8fa;
-    border-radius: 28px;
-    padding: 18px;
-    color: #233257;
-    margin-bottom: 16px;
-    font-size: 14px;
-    text-align: center;
-  }
-`;
+//   li {
+//     list-style: none;
+//     background: #f6f8fa;
+//     border-radius: 28px;
+//     padding: 18px;
+//     color: #233257;
+//     margin-bottom: 16px;
+//     font-size: 14px;
+//     text-align: center;
+//   }
+// `;
 
 const QuestionList = props => {
   return (
-    <Ul>
+    <ul className="question-list">
       {props.questions.map(question => (
         <li key={question.id}>{question.content}</li>
       ))}
-    </Ul>
+    </ul>
   );
 };
 
