4829cc4717376927820f33b7a9b597ded8e111d6
siwen.ren <siwen.ren@gaiaworks.cn>
2019-10-15 09:07:47 +0800

    test contentful api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Chat from "./pages/Chat";
+import TestContentful from "./pages/Contentful";
 import { ProvideMsg } from "./container/useMsg";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/" component={Welcome} exact />
           <Route path="/chat" component={Chat} />
+          <Route path="/test-contentful" component={TestContentful} />
         </Switch>
       </BrowserRouter>
     </ProvideMsg>
---
+++ src/pages/Contentful.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { createClient } from "contentful";
+import ReactMarkdown from "react-markdown";
+import "./Contentful.scss";
+
+const client = createClient({
+  space: "min1s6zlj815",
+  accessToken: "romx301L0RTIp4GVgtC3qIVRGTKL6FP2NSbCevfUtmk"
+});
+
+const TestContentful = () => {
+  const [posts, setPosts] = useState([]);
+
+  async function fetchContentTypes() {
+    const types = await client.getContentTypes();
+    if (types.items) return types.items;
+    console.log("Error getting Content Types.");
+  }
+
+  async function fetchEntriesForContentType(contentType) {
+    const entries = await client.getEntries({
+      content_type: contentType.sys.id
+    });
+    if (entries.items) return entries.items;
+    console.log(`Error getting Entries for ${contentType.name}.`);
+  }
+
+  useEffect(() => {
+    async function getPosts() {
+      const contentTypes = await fetchContentTypes();
+      const allPosts = await fetchEntriesForContentType(contentTypes[0]);
+      setPosts([...allPosts]);
+    }
+    getPosts();
+  }, []);
+
+  return (
+    <React.Fragment>
+      {posts.map(post => (
+        <div key={post.sys.id}>
+          <h1>{post.fields.title}</h1>
+          <h5>{post.fields.description}</h5>
+          <ReactMarkdown source={post.fields.project} />
+        </div>
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default TestContentful;
